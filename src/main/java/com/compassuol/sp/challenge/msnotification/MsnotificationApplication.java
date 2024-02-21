b1e1b2a1b352478cb5707ab07aab298a117ea992
Pedro Esteves <kropinho211@outlook.com>
2024-02-21 17:21:02 -0300

    Initial Commit

---
+++ src/main/java/com/compassuol/sp/challenge/msnotification/MsnotificationApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.msnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnotificationApplication.class, args);
+	}
+
+}
